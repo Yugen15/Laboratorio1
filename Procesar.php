c8a5846d98596bcf85e1817a9dcefc4fd080aa9c
Yugen15 <davidmendoza24072003@gmail.com>
2024-03-21 23:00:32 -0600

    add:Procesar

---
+++ Procesar.php
@@ -0,0 +1,12 @@
+<?php
+$respuesta = null;
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if($_SERVER["agregar"]){
+        $hw = $_REQUEST["#txtHw"];
+        $status = $_REQUEST["#chStatus"];
+        $descrip = $_REQUEST["txtDescription"];
+        
+        //Hola Jos muak
+    }
+}
