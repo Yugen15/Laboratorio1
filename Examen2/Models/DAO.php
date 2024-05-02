6720d22fa101f5f6550fdf6a0ee9803b4e77689e
Yugen15 <146015087+Yugen15@users.noreply.github.com>
2024-05-02 09:05:36 -0600

    Update DAO.php

--- Examen2/Models/DAO.php
+++ Examen2/Models/DAO.php
@@ -1,1 +1,50 @@
 <?php
+include_once "Config.php";
+include_once "Productos.php";
+
+class DAO{
+    private $con;
+
+    private function conectar(){
+        try{
+            $this->con = new mysqli(HOST,USER,PASS,DB);
+        }catch(Exception $ex){
+            echo "<script>console.log(".$ex->getTraceAsString().")</script>";
+        }
+    }
+    private function desconectar(){
+        $this->con->close();
+    }
+
+    public function get(){
+
+        $sql = "select * productos";
+        $this->conectar();
+        $resultado = $this->con->query($sql);
+        //Armar una tabla en HTML como resultado de este metodo
+
+        $html = "<table id='tabla' class='table table-striped table-dark'>";
+        $html .= "<thead><tr><th>ID</th><th>USUARIO</th><th>CONTRASEÑA</th><th>NIVEL</th><th>ACCIONES</th></tr></thead>";
+        $html .= "<tbody>";
+        while($fila=mysqli_fetch_assoc($resultado)){
+            $html .= "<tr>";
+            $html .= "<td>" . $fila['idproducto'] . "</td>";
+            $html .= "<td>" . $fila['idcategoria'] . "</td>"; 
+            $html .= "<td>" . $fila['nombre'] . "</td>";
+            $html .= "<td>" . $fila['costo'] . "</td>";
+            $html .= "<td>" . $fila['precio'] . "</td>";
+            $html .= "<td>" . $fila['stock'] . "</td>";
+            $html .= "<td> 
+            <img src='img/modificar.png' style='width:25px; height:25px; cursor: pointer;' onclick='editar(" . $fila['idusuario'] . ")'>
+                <a onclick=\"javascript:eliminar('" . $fila['idusuario'] . "')\" style='cursor:pointer;'>
+                <img src='img/eliminar.png' style='width:25px; height:25px;'> </a> 
+            </td>";
+            $html .= "</tr>";
+        }
+        $html .= "</tbody></table>";
+        $resultado->close();
+        $this->con->close();
+        return $html;
+    
+    }
+}
